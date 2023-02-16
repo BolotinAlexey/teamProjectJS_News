e305c0e4331ff14f3b4c291160035fea23571750
BolotinAlexey <62134373+BolotinAlexey@users.noreply.github.com>
2023-02-16 12:55:44 +0000

    Deploying to gh-pages from @ BolotinAlexey/teamProjectJS_News@1aa097367f59a3f49e1a32803e42fed038436327 🚀

---
+++ index.933a62ef.js
@@ -0,0 +1,2 @@
+({btnSearch:document.querySelector(".search__btn"),burger:document.querySelector(".menu__btn-burger")}).btnSearch.addEventListener("click",(function(e){e.currentTarget.parentNode.firstElementChild.classList.add("js-search-active"),e.currentTarget.classList.add("js-search-active")}));
+//# sourceMappingURL=index.933a62ef.js.map
---
+++ index.bc532ba5.js
@@ -0,0 +1,2 @@
+[document.querySelector(".search__btn"),document.querySelector(".menu__btn-burger")][0].addEventListener("click",(function(e){e.currentTarget.parentNode.firstElementChild.classList.add("js-search-active"),e.currentTarget.classList.add("js-search-active")}));
+//# sourceMappingURL=index.bc532ba5.js.map
