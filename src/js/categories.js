4ca783ba9b70b1108be54c0f1e063a87900c8716
Oleksii Bolotin <samkiral73@gmail.com>
2023-02-20 22:54:31 +0100

    fix first 6 words.length <10 & randomise

--- src/js/categories.js
+++ src/js/categories.js
@@ -4,6 +4,7 @@ import createStringList from './categories/createStringList';
 import createDropList from './categories/createDropList';
 import onDrop from './categories/onDrop';
 import onDropParent from './categories/onDropParent';
+import randomList from './categories/randomList';
 import { ref } from './categories/refCaregories';
 import ApiNews from './apiNews';
 import CATEGORIES_LENGTH from './constants';
@@ -18,25 +19,24 @@ export default function categores() {
 // doing for resize or reload
 function handlerCetegories(news) {
   // format returning from localStorage: "item1,item2,..."
-  const categList = localStorage.getItem('categList')?.split(',') || [];
-  if (categList.length < CATEGORIES_LENGTH)
-    getCategories(news, categList).then(list =>
-      createListManager(list, ref.list)
-    );
-  else createListManager(categList);
+  // const categList = localStorage.getItem('categList')?.split(',') || [];
+  // if (categList.length < CATEGORIES_LENGTH)
+  //   getCategories(news, categList).then(list =>
+  //     createListManager(list, ref.list)
+  //   );
+  // else createListManager(categList);
+
+  getCategories(news).then(list => createListManager(list));
 }
 
-async function getCategories(news, list) {
+async function getCategories(news) {
   const arrCategCommon = await news.categories;
-  arrCategCommon.length = CATEGORIES_LENGTH - list.length;
-  const arrCateg = arrCategCommon.map(el => el.display_name);
 
-  localStorage.setItem('categList', arrCateg);
-  return arrCateg;
+  return arrCategCommon.map(el => el.display_name);
+
+  // localStorage.setItem('categList', arrCateg);
 }
 
 function createListManager(list) {
-  const { windowWidth, windowHeight } = defineSizesWindow();
-  const countCategories = defineCountCategories(windowWidth);
-  createDropList(list);
+  createDropList([...randomList(list)]);
 }
--- src/js/categories/createDropList.js
+++ src/js/categories/createDropList.js
@@ -1,7 +1,9 @@
 import { ref } from './refCaregories';
-import CATEGORIES_DROP_LENGTH from '../constants';
+import { CATEGORIES_DROP_LENGTH } from '../constants';
 export default function createDropList(list) {
+  console.log(list);
   ref.arrBtns.forEach((el, i) => {
     if (!el.classList.contains('drop-btn')) el.textContent = list[i];
+    // console.log(el, list[i]);
   });
 }
